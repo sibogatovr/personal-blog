cecf163f0dd366256a4e7f8330d4e1833b5cb66e
sibogatovr <67776232+sibogatovr@users.noreply.github.com>
2023-05-25 14:14:31 +0500

    Add project files.

---
+++ 1stBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _1stBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
