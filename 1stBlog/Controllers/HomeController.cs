28580ec2b13c86f72bacc18f2b72f9d563b2165f
sibogatovr <67776232+sibogatovr@users.noreply.github.com>
2023-06-08 02:24:40 +0500

    Home and BlogPost

--- 1stBlog/Controllers/HomeController.cs
+++ 1stBlog/Controllers/HomeController.cs
@@ -42,6 +42,11 @@ namespace _1stBlog.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
